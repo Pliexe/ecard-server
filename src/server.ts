99ac5c7a0e8670f9bf78a73070230dba309fb2bb
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2021-02-14 18:17:07 +0100

    huh

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,34 @@ import http from 'http';
 const server = http.createServer();
 
 const wss = new WebSocket.Server({ server });
+const VERSION = 0.1;
+const PORT = 3000;
 
 wss.on('connection', (ws) => {
     console.log('CONNECTED');
 
     ws.on('message', (data) => {
-        console.log(`Data received: ${data}`);
-        ws.send(data);
+        let obj = JSON.parse(data.toString());
+        console.log(obj);
+
+        if (obj == null) return;
+        if (obj["name"] == null) return;
+
+        switch (obj.name) {
+            case "register":
+                if (obj["version"] == null) return;
+                break;
+        }
+
+        console.log(`Data received: ${obj}`);
+        ws.send(JSON.stringify(obj));
     });
 
-    ws.on('spawn', (data) => {
-        console.log(`Data received: ${data}`);
-        ws.send(data);
+    ws.on('close', (code, reason) => {
+        console.log("Disconnected");
     });
 });
 
-server.listen(3000, () => {
-    console.log('Listening to port');
+server.listen(PORT, () => {
+    console.log('Listening to port: ' + PORT);
 });
